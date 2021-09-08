8f9fbf87607283adde6da344f8efd50c20de2d2d
Jonas Gama <jonasgamaifsp@gmail.com>
2021-09-08 12:53:51 -0300

    cover the hello world controller

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.rxjava3.http.client.Rx3HttpClient;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class DemoTest {
@@ -13,9 +18,19 @@ class DemoTest {
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    @Client("/")
+    private Rx3HttpClient client;
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    public void shouldReceiveHelloWorld(){
+        String retrieve = client.retrieve("/hello").blockingLast();
+        assertEquals(retrieve,"Hello World");
     }
 
 }
