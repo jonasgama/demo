ce029ef96aefd936388f2306c20d6a0831d70197
Jonas Gama <jonasgamaifsp@gmail.com>
2021-09-12 15:58:49 -0300

    setup logging

--- src/main/java/com/example/service/HelloService.java
+++ src/main/java/com/example/service/HelloService.java
@@ -2,17 +2,29 @@ package com.example.service;
 
 import com.example.config.ConfigProps;
 import io.micronaut.context.annotation.Property;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class HelloService {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloService.class);
 
     @Property(name="hello.service.greeting", defaultValue = "Hello from service")
     private String greeting;
 
     @Inject
     private ConfigProps config;
+
+    @EventListener
+    public void onStartup(StartupEvent event){
+        LOG.debug("Service's been initialized");
+    }
 
     public String greeting(){
         return greeting;
